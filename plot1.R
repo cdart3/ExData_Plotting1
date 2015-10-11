3346d8c74349763fa61a9f8787ac2c30cb2a836a
unknown <dart2@illinois.edu>
2015-10-10 19:49:16 -0500

    upload

--- plot1.R
+++
@@ -1,8 +0,0 @@
-datafile <- "./household_power_consumption.txt"
-data <- read.table(datafile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-plotdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-globaldata <- as.numeric(plotdata$Global_active_power)
-png("plot1.png", width = 480, height = 480)
-hist(globaldata, col = "red", main = "Global Active Power", xlab = "Global Active Power (kW)")
-dev.off()
--- plot2.R
+++
@@ -1,9 +0,0 @@
-datafile <- "./household_power_consumption.txt"
-data <- read.table(datafile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-plotdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-date_time <- strptime(paste(plotdata$Date, plotdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globaldata <- as.numeric(plotdata$Global_active_power)
-png("plot2.png", width=480, height=480)
-plot(date_time, globaldata, type="l", xlab="", ylab="Global Active Power (kilowatts)")
-dev.off()
